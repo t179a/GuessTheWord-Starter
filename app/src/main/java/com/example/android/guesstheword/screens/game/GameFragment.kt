aa90625d408a81bc4e7d43bae82b56defe8b2722
t179a <azmtzk179@gmail.com>
2021-03-26 05:24:50 +0900

    Attach observers to the LiveData objects.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -24,6 +24,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
@@ -52,36 +53,33 @@ class GameFragment : Fragment() {
         )
         Log.i("GameFragment", "Called ViewModelProvider.get")
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
+            binding.wordText.text = newWord
+        })
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         binding.endGameButton.setOnClickListener { onEndGame()}
-        updateScoreText()
-        updateWordText()
+
         return binding.root
     }
 
 
     private fun onSkip() {
         viewModel.onSkip()
-        updateWordText()
-        updateScoreText()
+
     }
 
     private fun onCorrect() {
         viewModel.onCorrect()
-        updateScoreText()
-        updateScoreText()
+
     }
 
 
-    private fun updateWordText() {
-        binding.wordText.text = viewModel.word.value
-    }
 
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.value.toString()
-    }
 
     private fun onEndGame() {
         gameFinished()
